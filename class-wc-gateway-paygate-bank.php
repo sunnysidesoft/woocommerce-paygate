f9c706fb8a25e9f48fa6516b6f15d358a1dcdff8
yjiq150 <yjiq150@gmail.com>
2013-11-11 15:00:20 +0900

    .

--- class-wc-gateway-paygate-bank.php
+++ class-wc-gateway-paygate-bank.php
@@ -1,14 +1,14 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-class WC_Gateway_PayGate_mobile extends WC_Gateway_PayGate {
+class WC_Gateway_PayGate_bank extends WC_Gateway_PayGate {
         
     function __construct(){
                
-        $this->id					= 'paygatekorea_mobile';
+        $this->id					= 'paygatekorea_bank';
         $this->paygate_payment_method = '4';
-        $this->method_title			= __('Paygate(핸드폰소액결제)', 'sunnysidesoft');
-        $this->method_description   = 'paygatekorea_mobile';
+        $this->method_title			= __('Paygate(실시간계좌이체)', 'sunnysidesoft');
+        $this->method_description   = 'paygatekorea_bank';
         
 		if( $this->get_woocommerce_major_version() >= 2) {
 			$this->title 			= $this->get_option('title');
@@ -33,19 +33,19 @@ class WC_Gateway_PayGate_mobile extends WC_Gateway_PayGate {
 								'title' => __( '결제수단 명칭', 'sunnysidesoft' ),
 								'type' => 'text',
 								'description' => __( '결제 수단 선택 화면에서 출력될 결제 수단의 이름을 입력하세요.', 'sunnysidesoft' ),
-								'default' => __( '핸드폰소액결제', 'sunnysidesoft' )
+								'default' => __( '실시간계좌이체', 'sunnysidesoft' )
 							),
 				'description' => array(
 								'title' => __( '결제 수단 선택시 추가 설명 입력', 'sunnysidesoft' ),
 								'type' => 'textarea',
 								'description' => '',
-								'default' => __('핸드폰소액결제를 이용하여 결제합니다.', 'sunnysidesoft')
+								'default' => __('실시간계좌이체를 이용하여 결제합니다.', 'sunnysidesoft')
 							),
 				'thankyou_extra_message' => array(
 								'title' => __( '주문 완료 화면 추가 메시지', 'sunnysidesoft' ),
 								'type' => 'textarea',
 								'description' => __( '결제후 주문 완료화면에서 추가적으로 보여줄 메시지를 입력하세요.', 'sunnysidesoft' ),
-								'default' => __('핸드폰소액결제가 안전하게 처리되었습니다.', 'sunnysidesoft')
+								'default' => __('실시간 계좌이체가 안전하게 처리되었습니다.', 'sunnysidesoft')
 							)
             ));
     }
